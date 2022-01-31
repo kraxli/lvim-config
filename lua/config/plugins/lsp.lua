a878320095a8faf5666c5d0f455a211bb49d430f
kraxli <kraxli@pm.me>
2022-01-31 21:47:18 +0100

    lsp settings

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,5 +1,6 @@
 
-lvim.lsp.diagnostics.underline = false
+-- lvim.lsp.diagnostics.underline = false
+lvim.lsp.diagnostics.underline = true
 lvim.autocommands.custom_groups = {
   { "CursorHold", "*", "lua vim.diagnostic.open_float(0,{scope='line'})" },
 }
